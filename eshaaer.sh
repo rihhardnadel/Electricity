432069d32b72e6aaf905f5cc17d6aa195bdbb389
Rihhard Nadel <Rihhard@gmail.com>
2021-10-20 23:48:21 +0300

    Better

--- eshaaer.sh
+++ eshaaer.sh
@@ -1,3 +1,4 @@
 export FLASK_APP=app
+export FLASK_ENV=development 
 flask run &
 DISPLAY=:0 chromium-browser --kiosk localhost:5000
--- getelekter.py
+++ getelekter.py
@@ -1,11 +1,12 @@
 import requests
 import json
 from datetime import datetime, timedelta
+from dateutil import tz
 
 from requests.api import get
 
 def get_data():
-    today = datetime.now() - timedelta(hours=3)
+    today = datetime.now() - timedelta(hours=4)
     tomorrow = today + timedelta(days=1)
     actual_tomorrow = tomorrow.replace(hour=0)
 
@@ -15,8 +16,8 @@ def get_data():
     URL = f"https://dashboard.elering.ee/api/nps/price?start={start}&end={end}"
 
     response = requests.get(URL)
-
     data = json.loads(response.content.decode())["data"]["ee"]
+    
     return data
 
 
@@ -26,13 +27,16 @@ def format_data(data):
     new_data = []
 
     for datapoint in data:
-        actual_hour = datetime.utcfromtimestamp(datapoint["timestamp"]) + timedelta(hours=2)
+        utc_time = datetime.utcfromtimestamp(datapoint["timestamp"])
+        utc = utc_time.replace(tzinfo=tz.gettz("UTC"))
+
+        actual_hour = utc.astimezone(tz.tzlocal())
         dic = {"timestamp" : actual_hour.strftime("%H:%M"), "price" : datapoint["price"]}
         new_data.append(dic)
 
-    print(new_data)
     return new_data
 
 if __name__ == "__main__":
     data = get_data()
     new_data = format_data(data)
+    print(new_data)
